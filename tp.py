d395622fd65481268e34e2a9476f5e70c7fb6fc8
Théo <theo_38180@live.fr>
2021-02-14 19:37:24 +0100

    fragmentation fonction writeEndHtml

--- tp.py
+++ tp.py
@@ -46,6 +46,11 @@ def getBorne():
 firstBorne, firstBorneUrl = getBorne()
 lastBorne, lastBorneUrl = getBorne()
 
+firstBorne = "Gnocchi"
+firstBorneUrl = "Gnocchi"
+lastBorne = "Italie"
+lastBorneURL = "Italie"
+
 def checkEndGame(title):
     global firstBorne
     global firstBorneUrl
@@ -194,6 +199,22 @@ def writeGoBackhtml(file):
         </div>
         """.format(allTitlesVisited[-2], allLinksVisited[-2])) #allLinksVisited[-1] représente le dernier lien du tableau de tous les liens parcouru
 
+def writeEndHtml(file, allGoodLinksUnique, allGoodLinksUrl, allGoodLinksTitre):
+    file.write("""
+        <article class="wiki-links">""")
+    for i in range(len(allGoodLinksUnique)):
+        try:
+            file.write("""        <section onclick='nextPage(`{}`)'><p>{}</p></section>\n""".format(allGoodLinksUrl[i], allGoodLinksTitre[i]))
+        except:
+            print("Erreur de out of range, mais je sais pas pourquoi ça a lieu")
+    file.write("""
+    </article>
+            <script src="js/main.js"></script>
+        </body>
+    </html>
+    """)
+    file.close()
+
 #TODO : terminer la fonction openPage et faire celle pour afficher tous les trucs tavu et continuer la fonction chelou de loadpage
 def loadpage():
     global currentPage
@@ -228,28 +249,10 @@ def loadpage():
     writeHtml(f, newTitle, counter, descdescriptionFinal)
     if len(allLinksVisited) > 1:
         writeGoBackhtml(f)
-    
-    f.write("""
-        <article class="wiki-links">""")
-    for i in range(len(allGoodLinksUnique)):
-        try:
-            f.write("""        <section onclick='nextPage(`{}`)'><p>{}</p></section>\n""".format(allGoodLinksUrl[i], allGoodLinksTitre[i]))
-        except:
-            print("Erreur de out of range, mais je sais pas pourquoi ça a lieu")
-    f.write("""
-    </article>
-            <script src="js/main.js"></script>
-        </body>
-    </html>
-    """)
-    f.close()
+    writeEndHtml(f, allGoodLinksUnique, allGoodLinksUrl, allGoodLinksTitre)
+
     if not firstLoad:
-        try:
-            eel.reloadPage()
-        except:
-            print("YA EU L'ERREUR AAAAAAAAAAAAAA")
-            time.sleep(1)
-            getLinks(currentUrl)
+        eel.reloadPage()
     else:
         firstLoad = False
 
@@ -267,173 +270,6 @@ def initiate(Linkurl):
 
     global newTitle
     checkEndGame(newTitle)
-
-#GetLinks permet à partir d'un lien wiki de générer une page html avec tous les liens et d'autres trucs
-@eel.expose #Pour qu'on puisse appeller la fonction dans le js de l'html que l'on génère
-def getLinks(borneUrl, counterPoints = 1, addTab = True): #la borneUrl est en réalité la fin de l'url d'une page wikipedia
-    with urllib.request.urlopen(formatLink(borneUrl)) as response:
-        webpage = response.read()
-        soup = BeautifulSoup(webpage, 'html.parser')
-        
-        global newTitle
-        global counter
-        global firstLoad
-
-        global firstBorne
-        global firstBorneUrl
-        global lastBorne
-        global lastBorneUrl
-
-        if(newTitle == lastBorne):
-            if(firstLoad):
-                firstBorne, firstBorneUrl = getBorne()
-                lastBorne, lastBorneUrl = getBorne()
-                getLinks(firstBorneUrl)
-            else:
-                print("Victoire !")
-                endGame()
-        else:
-            div = soup.find("div", {"id": "mw-content-text"})
-            allLinks = div.find_all("a")
-
-            toolBox = div.find_all("div", {"class": "bandeau-container"})
-            if(toolBox == None):
-                print("pas de bandeau détecté, tout va bien")
-            else:
-                print("bandeau détecté, on le tej")
-                for i in range(len(toolBox)):
-                    toolBox[i].decompose()
-
-
-            asideMenu = div.find("div", {"class": "infobox_v3"})
-            if(asideMenu == None):
-                print("Pas de menu v3 détecté, tranquille")
-                asideMenu = div.find("table", {"class": ["infobox_v2", "infobox", "DebutCarte"]})
-                if(asideMenu != None):
-                    asideMenu.decompose()
-            else:
-                print("Menu v3 détecté, on le tej")
-                asideMenu.decompose()
-
-            description2 = div.find("p", {"class": None})
-            #print(description2)
-
-            descriptionStyle = description2.find("style")
-            if(descriptionStyle != None):
-                descriptionStyle.decompose()
-
-            description = description2.find_all(text=True)
-            descriptionFinal = []
-
-            descCounter = 0
-            for i in description:
-                iString = str(i)
-                if not((iString == '[' and description[descCounter + 1].isdigit()) or iString.isdigit() or (iString == ']' and description[descCounter - 1].isdigit()) or (iString == ',' and description[descCounter - 1] == ']' and description[descCounter + 1] == '[')):
-                    descriptionFinal.append(i)
-                descCounter = descCounter + 1
-
-            
-            descdescriptionFinal = ''.join(descriptionFinal)
-            #print(descdescriptionFinal)
-            descdescriptionFinal = descdescriptionFinal.replace("(Écouter)", '')
-            descdescriptionFinal = descdescriptionFinal.replace(" ,", ',')
-            #print(descdescriptionFinal)
-            
-            allGoodLinks = []
-            for i in allLinks:
-                iString = str(i)
-                if "/wiki/" in iString and ':' not in iString and "src=" not in iString:
-                    allGoodLinks.append(i)
-
-            allGoodLinksUnique = list(dict.fromkeys(allGoodLinks))
-
-            allGoodLinksTitre = []
-            for i in allGoodLinksUnique:
-                text = str(i)
-                m = re.search('>(.+?)</a>', text)
-                if m:
-                    allGoodLinksTitre.append(m.group(1)) 
-            
-            allGoodLinksUrl = []
-            for i in allGoodLinksUnique:
-                text = str(i)
-                m = re.search('/wiki/(.+?)"', text)
-                if m:
-                    allGoodLinksUrl.append(m.group(1))
-
-            if addTab:
-                print("Génération du tableau : {}".format(borneUrl))
-                allLinksVisited.append(borneUrl)
-                allTitlesVisited.append(newTitle)
-            else:
-                allLinksVisited.pop()
-                allTitlesVisited.pop()
-
-            print(allTitlesVisited)
-            #création de l'html et insertion des données
-            counter = counter + counterPoints
-            currentPath = os.path.dirname(__file__)
-            currentPath = os.path.join(currentPath,"wiki.html")
-            f = open(currentPath,'w+', encoding='utf-8')
-            f.write("""
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Wikigame</title>    
-                <link rel="preconnect" href="https://fonts.gstatic.com">
-                <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=PT+Serif&display=swap" rel="stylesheet">
-                <link rel="stylesheet" href="style.css">
-                <script src="https://kit.fontawesome.com/373a1c097b.js" crossorigin="anonymous"></script>
-                <script type="text/javascript" src="/eel.js"></script>
-                <script src="js/reload.js"></script>
-            </head>
-                <body>
-                <aside class="loading hide">Chargement...</aside>
-                <nav>
-                    <p>{} > {}<p/>
-                </nav>
-                <header>
-                    <section>
-                        <h1>{}</h1>
-                    </section>
-                    <section>
-                        <h1 class="counter">Coup n°{}</h1>
-                    </section>
-                </header>
-                <p class="description">{}<p>""".format(firstBorne, lastBorne, newTitle, counter, descdescriptionFinal))
-            if len(allLinksVisited) > 1:
-                f.write("""
-                <div class='previous-div'>
-                    <h2>Page précédente : {}</h2>
-                    <button onclick="goBackJS(`{}`)"><i class="fas fa-backward"></i>Revenir en arrière ? (coute deux coups)</button>
-                </div>
-                """.format(allTitlesVisited[-2], allLinksVisited[-2])) #allLinksVisited[-1] représente le dernier lien du tableau de tous les liens parcouru
-            f.write("""
-                <article class="wiki-links">""")
-            for i in range(len(allGoodLinksUnique)):
-                try:
-                    f.write("""        <section onclick='nextPage(`{}`)'><p>{}</p></section>\n""".format(allGoodLinksUrl[i], allGoodLinksTitre[i]))
-                except:
-                    print("Erreur de out of range, mais je sais pas pourquoi ça a lieu")
-            f.write("""
-            </article>
-                    <script src="js/main.js"></script>
-                </body>
-            </html>
-            """)
-            f.close()
-            if not firstLoad:
-                try:
-                    eel.reloadPage()
-                except:
-                    print("YA EU L'ERREUR AAAAAAAAAAAAAA")
-                    time.sleep(1)
-                    getLinks(borneUrl)
-            else:
-                firstLoad = False
-            print("\n\n\n")
 
 @eel.expose
 def goBack(url):
